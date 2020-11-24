f165d9863375687d5b69ac49b527611508b53ce5
Maleriya <glen9234@yandex.ru>
2020-11-24 19:01:06 +0300

    HW08

---
+++ ElenaNedorezovaLesson08/ElenaNedorezovaLesson08_HW01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElenaNedorezovaLesson08_HW01
+{
+    /*
+    Задание:
+    С помощью рефлексии выведите все свойства структуры DateTime
+
+    Студент:
+    Елена Недорезова
+    */
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime();
+            MemberInfo[] memberInfos = dateTime.GetType().GetMembers();
+
+            foreach (var item in memberInfos)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ ElenaNedorezovaLesson08/ElenaNedorezovaLesson08_HW03/TrueFalse.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace ElenaNedorezovaLesson08_HW03
+{
+    // Класс для вопроса    
+    [Serializable]
+    public class Question
+    {
+        public string text;
+        public bool trueFalse;
+
+        public Question()
+        {
+        }
+        public Question(string text, bool trueFalse)
+        {
+            this.text = text;
+            this.trueFalse = trueFalse;
+        }
+    }
+
+    class TrueFalse
+    {
+        string fileName;
+        List<Question> list;
+        public string FileName
+        {
+            set { fileName = value; }
+        }
+        public TrueFalse(string fileName)
+        {
+            this.fileName = fileName;
+            list = new List<Question>();
+        }
+        public void Add(string text, bool trueFalse)
+        {
+            list.Add(new Question(text, trueFalse));
+        }
+        public void Remove(int index)
+        {
+            if (list != null && index < list.Count && index >= 0) list.RemoveAt(index);
+        }
+
+        public Question this[int index]
+        {
+            get { return list[index]; }
+        }
+        public void Save()
+        {
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<Question>));
+            Stream fStream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            xmlFormat.Serialize(fStream, list);
+            fStream.Close();
+        }
+        public bool Load()
+        {
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<Question>));
+            Stream fStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            bool success = true;
+            try
+            {
+                list = (List<Question>)xmlFormat.Deserialize(fStream);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"С чтением файла что-то не так {ex.Message}","Ошибка");
+                success = false;  
+            }
+            finally
+            {
+                fStream.Close();
+            }
+
+            return success;
+        }
+        public int Count
+        {
+            get { return list.Count; }
+        }
+    }
+
+}
